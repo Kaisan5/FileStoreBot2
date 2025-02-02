d911c1dbecb3737ffbdd698de08b0552a4147f09
Kai <131925584+Kaisan5@users.noreply.github.com>
2025-02-02 18:49:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,19 +68,6 @@ class Bot(Client):
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCESUB_CHANNEL3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCESUB_CHANNEL3}")
                 self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/weebs_support for support")
-                sys.exit()
-        if FORCESUB_CHANNEL4:
-            try:
-                link = (await self.get_chat(FORCESUB_CHANNEL4)).invite_link
-                if not link:
-                    await self.export_chat_invite_link(FORCESUB_CHANNEL4)
-                    link = (await self.get_chat(FORCESUB_CHANNEL4)).invite_link
-                self.invitelink3 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCESUB_CHANNEL4 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCESUB_CHANNEL4}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/weebs_support for support")
                 sys.exit()                 
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
